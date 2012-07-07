49941c3074e4288e8f6e96d95e9444a444067b3d
Daniel D. Shaw <github@dshaw.com>
2012-07-07 00:40:35 -0700

    Add travis-ci support.

---
+++ test/timing.test.js
@@ -0,0 +1,31 @@
+var Timing = require('..')
+  , Timer = Timing.Timer
+  , test = require('tap').test
+
+test('Timing', function (t) {
+  t.ok(Timing, 'exports Timing')
+  t.ok(Timer, 'exports Timer')
+
+  var timing = new Timing
+
+  t.isa(timing, Timing, 'timing is an instance of Timing')
+  t.ok(timing.options, 'timing exposes options')
+  t.ok(timing.timers, 'initialized timers hash')
+
+  t.end()
+})
+
+test('timing timer', function (t) {
+  t.plan(1)
+
+  var timing = new Timing
+    , timeout = 500
+
+  timing.time('hammer')
+
+  setTimeout(function () {
+    var timer = timing.timeEnd('hammer')
+    t.equal(Math.round(timer.duration/10), Math.round(timeout/10), 'timer duration')
+    t.end()
+  }, timeout)
+})
---
+++ timing.js
@@ -0,0 +1,102 @@
+/*!
+ * timing
+ * Copyright(c) 2012 Daniel D. Shaw <dshaw@dshaw.com>
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var util = require('util')
+  , EventEmitter = require('events').EventEmitter
+
+/**
+ * Configuration.
+ */
+
+var Time = Date // yep, that's how we roll
+  , debug = function () {}
+
+/**
+ * Exports.
+ */
+
+module.exports = Timing
+Timing.Timer = Timer
+
+
+/**
+ * Timing
+ *
+ * @param options
+ * @constructor
+ */
+
+function Timing (options) {
+  options || (options = {})
+
+  if (options.debug) debug = console
+  if (options.microtime) Time = require('microtime')
+
+  this.options = options
+  this.timers = {}
+}
+
+util.inherits(Timing, EventEmitter)
+
+Timing.prototype.time = function (label, options) {
+  options || (options = {})
+
+  if (this.timers[label] && !options.force) return this.timers[label]
+
+  this.timers[label] = new Timer(label)
+}
+
+Timing.prototype.timeEnd = function(label) {
+  var self = this
+    , timer = this.timers[label]
+  if (!timer) throw new Error('No such label: ' + label)
+  timer.on('end', function () {
+    self.emit('end', timer)
+  })
+  timer.timeEnd()
+  return timer
+ }
+
+Timing.prototype.clear = function (label) {
+  if (this.timers[label]) {
+    delete this.timers[label]
+    return true
+  }
+  return false
+}
+
+
+/**
+ * Timer
+ *
+ * @param options
+ * @constructor
+ */
+
+function Timer (label) {
+  if (!label) throw new Error('Timer needs a label: ' + label)
+
+  this.label = label
+  this.start = Time.now()
+  this.end = null
+  this.duration = null
+
+  this.emit('start', this.start)
+}
+
+util.inherits(Timer, EventEmitter)
+
+Timer.prototype.timeEnd = function() {
+  this.end = Time.now()
+  this.duration = this.end - this.start
+  this.emit('end', this.duration)
+
+  debug('%s: %dms', this.label, this.duration)
+}
